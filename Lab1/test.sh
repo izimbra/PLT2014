c9c48a66e191e64ec7a045c999d23d1d3aae90f1
Eugene Groshev <eugene.groshev@gmail.com>
2014-01-28 16:42:12 +0100

    Parsing file 3 + changes in test script and gitignore

--- Lab1/test.sh
+++ Lab1/test.sh
@@ -3,4 +3,24 @@
 make distclean
 bnfc -m --haskell pltCPP.cf
 make
+echo ""
+echo "Running test 1:"
 ./TestpltCPP hello.cc
+echo ""
+echo "Done!"
+echo ""
+echo ""
+echo "Running test 2:"
+./TestpltCPP greet.cc
+echo ""
+echo "Done!"
+echo ""
+echo ""
+echo "Running test 3:"
+./TestpltCPP med.cc
+echo ""
+echo "Done!"
+echo ""
+echo ""
+
+
