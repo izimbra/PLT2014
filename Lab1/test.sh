bfd554f595679bbc1107a2149d486e7ac35ec632
Eugene Groshev <eugene.groshev@gmail.com>
2014-02-03 23:45:58 +0100

    34 good and 135 bad tests pass, problems with `struct`

--- Lab1/test.sh
+++ Lab1/test.sh
@@ -4,41 +4,44 @@ make distclean
 bnfc --haskell pltCPP.cf
 make
 echo ""
-echo "Running test 1:"
-./TestpltCPP hello.cc
-echo ""
-echo "Done!"
-echo ""
-echo ""
-echo "Running test 2:"
-./TestpltCPP greet.cc
-echo ""
-echo "Done!"
-echo ""
-echo ""
-echo "Running test 3:"
-./TestpltCPP med.cc
-echo ""
-echo "Done!"
-echo ""
-echo ""
-echo "Running test 4:"
-./TestpltCPP grade.cc
-echo ""
-echo "Done!"
-echo ""
-echo ""
-echo "Running test 5:"
-./TestpltCPP palin.cc
-echo ""
-echo "Done!"
-echo ""
-echo ""
-echo "Running test 6:"
-./TestpltCPP grammar.cc
-echo ""
-echo "Done!"
-echo ""
-echo ""
+echo "Running test:"
+./TestpltCPP $1
+# echo ""
+# echo "Running test 1:"
+# ./TestpltCPP hello.cc
+# echo ""
+# echo "Done!"
+# echo ""
+# echo ""
+# echo "Running test 2:"
+# ./TestpltCPP greet.cc
+# echo ""
+# echo "Done!"
+# echo ""
+# echo ""
+# echo "Running test 3:"
+# ./TestpltCPP med.cc
+# echo ""
+# echo "Done!"
+# echo ""
+# echo ""
+# echo "Running test 4:"
+# ./TestpltCPP grade.cc
+# echo ""
+# echo "Done!"
+# echo ""
+# echo ""
+# echo "Running test 5:"
+# ./TestpltCPP palin.cc
+# echo ""
+# echo "Done!"
+# echo ""
+# echo ""
+# echo "Running test 6:"
+# ./TestpltCPP grammar.cc
+# echo ""
+# echo "Done!"
+# echo ""
+# echo ""
 
 
