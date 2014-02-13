d9775a6de8aa33efcff522f7bcedf8e6af5d375d
Emil <emilbzm@gmail.com>
2014-02-14 00:37:58 +0100

    temp push for sleep . not working. import TypeChecker with CHCI and continuee from there

--- Lab2/mini/haskell/TypeChecker.hs
+++ Lab2/mini/haskell/TypeChecker.hs
@@ -21,7 +21,15 @@ emptySig =  Map.empty
 -- until here added from book p72 and self-made empty functions. 
 
 typecheck :: Program -> Sig  -- first pass. get the signatures of functions by running through 
-typecheck (Prog defs)  = emptySig --getsignatures emptySig defs
+typecheck (Prog defs)  = getSignatures emptySig defs
+
+getSignatures :: Sig -> [Def] -> Sig
+getSignatures sig [] = sig  --base case returns finished sig
+getSignatures sig (de:defs) = do sig' <- getSign sig de
+				 getSignatures sig' defs
+
+getSign :: Sig -> Def -> Sig --get a Sig and update with given Def
+getSign sig (DFun typ id args stms) = Map.insert id ([] , typ) sig
 
 
 --typecheck :: Program -> Err ()
