be39f1543a9de36cb28c7201620b68ce98aa6504
trollol <emilbzm@gmail.com>
2014-02-14 15:54:07 +0100

    small fixes to make things work together parenthesis in TypeChcker.hs, void type in grammar some minor changes in type names etc, in BuiltinFunctions.hs

--- Lab2/TypeChecker.hs
+++ Lab2/TypeChecker.hs
@@ -1,22 +1,82 @@
 module TypeChecker where
 
-import AbsCPP -- former mini
-import PrintCPP -- former mini 
+import qualified Data.Map as M
+import AbsCPP
+import PrintCPP
 import ErrM
+import BuiltInFuncs
 
-type Env = [[(Id, Type)]]
+-- We get Printer and ErrM monad from BNFC.
+--
+-- Type checker --
+-- 2 passes
+-- 1) build symb. table, should include built-in functions
+-- 2) do type-checking and annotation
+--
+-- only 4 types needed (void is practical)
+--
+-- Program - list of defs. (functions only)
+-- Should be rules for built-ins
+--
+-- Checking return statment is optional
+-- When checking the func. def, we can carry the function type around during recursion)
+--
+-- We need to check that vars in arg.list are distinct
+--
+-- t x = e; can be viewed as synt. sugar, it can be replaceb by 2 statements:
+-- 1) t x;
+-- 2) x = e ;
 
-typecheck :: Program -> Err ()
-typecheck (Prog defs) = checkDefs emptyEnv defs
+-- check EApp f es 
+--  (ts, t) <- lookUp f
+--  smart way is to use zip:
+--  let ets = zip es ts - but will truncate the longer list!
+--  mapM (\(e,t) -> check e t) ets
 
-checkDefs :: Env -> [Def] -> Err ()
+-- | The environment of the type checker.
+-- Includes symbol table for functions and list of variable contexts.
+type Env = (SigTab, [Context]) -- mini version: [[(Id, Type)]]
+
+-- | Symbol table for functions .
+-- A map of function ids and their type signatures.
+type SigTab = M.Map Id Sig
+
+-- | Function type signature. Includes argument types and return type.
+type Sig = ([Type], Type)
+
+-- | Variable context - map of variable ids to their types
+type Context =  M.Map Id Type
+
+typecheck :: Program -> Err () -- Program
+typecheck (Prog defs) = do
+            env <- buildFunTable emptyEnv (defs ++ builtInFunctions)
+            return ()
+  --checkDefs emptyEnv defs
+
+-- | Builds a symbol table for functions in the environment.
+buildFunTable :: Env -> [Def] -> Err Env -- or just SigTab
+buildFunTable env []   = return env
+buildFunTable env (d:ds) =
+    case d of
+      Func ftype id args _ -> do env' <- updateFun env id (map argType args, ftype)
+                                 buildFunTable env' ds                                 
+      _                    -> fail "Bad function definition"
+    
+                    
+argType :: Arg -> Type
+argType (ADecl atype _) = atype
+
+checkDefs :: Env -> [Def] -> Err () -- Err Type?
 checkDefs env []     = return ()
 checkDefs env (d:ds) = do env' <- checkDef env d
                           checkDefs env' ds
 
-checkDef = undefined
+checkDef :: Env -> Def -> Err Env
+checkDef env d =
+    case d of
+      Func t f args _ -> undefined
 
-checkStms :: Env -> [Stm] -> Err ()
+checkStms :: Env -> [Stm] -> Err () -- (Env!)
 checkStms env [] = return ()
 checkStms env (st:stms) = do env' <- checkStm env st
                              checkStms env' stms
@@ -24,7 +84,7 @@ checkStms env (st:stms) = do env' <- checkStm env st
 checkStm :: Env -> Stm -> Err Env
 checkStm env s = 
     case s of
-      SDecl t x       -> addVar env x t
+      SDecl t x       -> updateVar env x t
       SAss x e        -> do t <- lookupVar env x
                             checkExp env e t
                             return env      
@@ -33,13 +93,16 @@ checkStm env s =
       SPrint e        -> do inferExp env e
                             return env
 
-checkExp :: Env -> Exp -> Type -> Err ()
+checkExp :: Env -> Exp -> Type -> Err () -- Err Exp
 checkExp env e t = 
     do t' <- inferExp env e
        if t' /= t 
          then fail (printTree e ++ " has type " ++ printTree t'
                     ++ " expected " ++ printTree t)
-         else return ()
+         else return () -- ETyped r t
+                        -- ETyped can be defined in the grammar
+                        -- as internal rule ETyped Exp ::= "(" Type ")" Exp1
+                        -- Parse will fail, but we will have the info in AST
 
 inferExp :: Env -> Exp -> Err Type
 inferExp env e = 
@@ -58,18 +121,35 @@ inferExp env e =
 
 
 emptyEnv :: Env
-emptyEnv = [[]]
+emptyEnv = (M.empty, [])
 
-addVar :: Env -> Ident -> Type -> Err Env
-addVar (scope:rest) x t = 
-    case lookup x scope of
-      Nothing -> return (((x,t):scope):rest)
+-- | Adds a function signature to the environment,
+-- or updates it if the function is already present.
+updateFun :: Env -> Id -> Sig -> Err Env
+updateFun (funs, scopes) id sig = let funs' = M.insert id sig funs
+                                  in  return (funs', scopes)
+
+-- | Looks up a function definition in the environment
+lookupFun :: Env -> Id -> Err Sig
+lookupFun (funs, _) f =
+  case M.lookup f funs of
+    Just sig -> return sig
+    Nothing  -> fail ("Undefined function " ++ printTree f ++ ".")
+
+-- | Adds a new variable to the current variable scope,
+-- or updates an existing variable    
+updateVar :: Env -> Id -> Type -> Err Env
+updateVar (funs, scope:rest) x t = 
+    case M.lookup x scope of
+      Nothing -> return (funs, (M.insert x t scope):rest)
       Just _  -> fail ("Variable " ++ printTree x ++ " already declared.")
 
-lookupVar :: Env -> Ident -> Err Type
-lookupVar [] x = fail $ "Unknown variable " ++ printTree x ++ "."
-lookupVar (scope:rest) x = case lookup x scope of
-                             Nothing -> lookupVar rest x
+-- | Looks up a variable in the environment
+lookupVar :: Env -> Id -> Err Type
+lookupVar (_, scopes) = lookup_ scopes 
+lookup_ [] x = fail $ "Unknown variable " ++ printTree x ++ "."
+lookup_ (scope:rest) x = case M.lookup x scope of
+                             Nothing -> lookup_ rest x
                              Just t  -> return t
 addScope :: Env -> Env
-addScope env = []:env
+addScope (funs, scopes) = (funs, M.empty:scopes)
