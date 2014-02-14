d8aa2cc84e02bf111f3704c9bbb11cf42ad960bf
trollol <emilbzm@gmail.com>
2014-02-14 03:59:19 +0100

    got the recursive Sig finder working with WHERE instead of DO. I don't understand why though. Good question to ask

--- Lab2/mini/haskell/TypeChecker.hs
+++ Lab2/mini/haskell/TypeChecker.hs
@@ -18,16 +18,29 @@ emptyEnv =  (emptySig, [] )
 emptySig :: Sig
 emptySig =  Map.empty
 
+def :: Def
+def = DFun TInt (Id "hej") [] []
+def2 :: Def
+def2 = DFun TInt (Id "hej2") [] []
+def3 :: Def
+def3 = DFun TInt (Id "hej3") [] []
+
+ds = [def, def2, def3]
+s = emptySig
+
 -- until here added from book p72 and self-made empty functions. 
 
 typecheck :: Program -> Sig  -- first pass. get the signatures of functions by running through 
 typecheck (Prog defs)  = getSignatures emptySig defs
 
 getSignatures :: Sig -> [Def] -> Sig
-getSignatures sig [] = sig  --base case returns finished sig
-getSignatures sig (de:defs) = do sig' <- getSign sig de
-				 getSignatures sig' defs
-
+getSignatures sig []        = sig  --base case returns finished sig
+getSignatures sig (de:defs) = getSignatures sig' defs
+    where
+        sig' = getSign sig de
+--getSignatures sig (de:defs) = do sig' <- getSign sig de
+--                                 getSignatures sig' defs
+                                 
 getSign :: Sig -> Def -> Sig --get a Sig and update with given Def
 getSign sig (DFun typ id args stms) = Map.insert id ([] , typ) sig
 
