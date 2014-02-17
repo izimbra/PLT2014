975432bc28db8f3d95dfa7ba8b865babfa1c096e
Eugene Groshev <eugene.groshev@gmail.com>
2014-02-17 10:20:14 +0100

    Script now runs checks on all `.cc` tests

--- Lab2/test.sh
+++ Lab2/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-TESTS="testsuite/good/core001.cc"
+TESTS='testsuite/good/core001.cc'
+GOODS='testsuite/good'
+BADS='testsuite/bad'
 
 # 2, 3, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19, 20,
 # 102, 
@@ -10,5 +12,16 @@ make all
 
 ghc --make -o lab2 lab2.hs
 
-./lab2 $TESTS
+echo 'Type checking good tests...'
+for FILE in `find $GOODS | grep -v -e '.cc.output' -e '.cc.input'`; do
+    echo `basename $FILE && ./lab2 $FILE`
+    
+done
+
+echo 'Type checking bad tests...'
+for FILE in `find $BADS | grep -v -e '.cc.output' -e '.cc.input'`; do
+    echo `basename $FILE && ./lab2 $FILE`
+    
+done
+
 
