9c47505ca710af7559a5e7c52301d3e218dcd836
trollol <emilbzm@gmail.com>
2014-02-17 18:27:47 +0100

    separated environmental issues from the typechecker code, so that we can reuse environment inside interpreter

---
+++ Lab2/Environment.hs
@@ -0,0 +1,59 @@
+module Environment where
+
+import qualified Data.Map as M
+import AbsCPP
+import PrintCPP
+import ErrM
+
+-- | The environment of the type checker.
+-- Includes symbol table for functions and list of variable contexts.
+type Env = (SigTab, [Context]) -- mini version: [[(Id, Type)]]
+
+-- | Symbol table for functions .
+-- A map of function ids and their type signatures.
+type SigTab = M.Map Id Sig
+
+-- | Function type signature. Includes argument types and return type.
+type Sig = ([Type], Type)
+
+-- | Variable context - map of variable ids to their types
+type Context =  M.Map Id Type
+
+
+
+-- | Looks up a function definition in the environment
+lookupFun :: Env -> Id -> Err Sig
+lookupFun (funs, _) f =
+  case M.lookup f funs of
+    Just sig -> return sig
+    Nothing  -> fail ("Undefined function " ++ printTree f ++ ".")
+
+-- | Adds a new variable to the current variable scope,
+-- or updates an existing variable    
+updateVar :: Env -> Id -> Type -> Err Env
+updateVar (funs, scope:rest) x t = 
+    case M.lookup x scope of
+      Nothing -> return (funs, (M.insert x t scope):rest)
+      Just _  -> fail ("Variable " ++ printTree x ++ " already declared.")
+
+
+-- | Looks up a variable in the environment
+lookupVar :: Env -> Id -> Err Type
+lookupVar (_, scopes) = lookup_ scopes 
+lookup_ [] x = fail $ "Unknown variable " ++ printTree x ++ "."
+lookup_ (scope:rest) x = case M.lookup x scope of
+                             Nothing -> lookup_ rest x
+                             Just t  -> return t
+addScope :: Env -> Env
+addScope (funs, scopes) = (funs, M.empty:scopes)
+
+
+emptyEnv :: Env
+emptyEnv = (M.empty, [])
+
+-- | Adds a function signature to the environment,
+-- or updates it if the function is already present.
+updateFun :: Env -> Id -> Sig -> Err Env
+updateFun (funs, scopes) id sig = let funs' = M.insert id sig funs
+                                  in  return (funs', scopes)
+
--- Lab2/TypeChecker.hs
+++ Lab2/TypeChecker.hs
@@ -1,11 +1,11 @@
 module TypeChecker where
 
-import qualified Data.Map as M
+--import qualified Data.Map as M
 import AbsCPP
 import PrintCPP
 import ErrM
 import BuiltInFuncs
-
+import Environment
 -- We get Printer and ErrM monad from BNFC.
 --
 -- Type checker --
@@ -32,20 +32,6 @@ import BuiltInFuncs
 --  smart way is to use zip:
 --  let ets = zip es ts - but will truncate the longer list!
 --  mapM (\(e,t) -> check e t) ets
-
--- | The environment of the type checker.
--- Includes symbol table for functions and list of variable contexts.
-type Env = (SigTab, [Context]) -- mini version: [[(Id, Type)]]
-
--- | Symbol table for functions .
--- A map of function ids and their type signatures.
-type SigTab = M.Map Id Sig
-
--- | Function type signature. Includes argument types and return type.
-type Sig = ([Type], Type)
-
--- | Variable context - map of variable ids to their types
-type Context =  M.Map Id Type
 
 typecheck :: Program -> Err () -- Program
 typecheck (Prog defs) = do
@@ -206,37 +192,3 @@ inferFunHelper env (e:es) (t:ts) = do etyp <- inferExp env e
                                         else fail "type error in argument of function call" 
 inferFunHelper _ _ _             = fail "inferFunHelper has non exhaustive case pattern"                  
 
-emptyEnv :: Env
-emptyEnv = (M.empty, [])
-
--- | Adds a function signature to the environment,
--- or updates it if the function is already present.
-updateFun :: Env -> Id -> Sig -> Err Env
-updateFun (funs, scopes) id sig = let funs' = M.insert id sig funs
-                                  in  return (funs', scopes)
-
--- | Looks up a function definition in the environment
-lookupFun :: Env -> Id -> Err Sig
-lookupFun (funs, _) f =
-  case M.lookup f funs of
-    Just sig -> return sig
-    Nothing  -> fail ("Undefined function " ++ printTree f ++ ".")
-
--- | Adds a new variable to the current variable scope,
--- or updates an existing variable    
-updateVar :: Env -> Id -> Type -> Err Env
-updateVar (funs, scope:rest) x t = 
-    case M.lookup x scope of
-      Nothing -> return (funs, (M.insert x t scope):rest)
-      Just _  -> fail ("Variable " ++ printTree x ++ " already declared.")
-
-
--- | Looks up a variable in the environment
-lookupVar :: Env -> Id -> Err Type
-lookupVar (_, scopes) = lookup_ scopes 
-lookup_ [] x = fail $ "Unknown variable " ++ printTree x ++ "."
-lookup_ (scope:rest) x = case M.lookup x scope of
-                             Nothing -> lookup_ rest x
-                             Just t  -> return t
-addScope :: Env -> Env
-addScope (funs, scopes) = (funs, M.empty:scopes)
