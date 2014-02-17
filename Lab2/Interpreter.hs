07272888abeea17b80df80ceafdf0f6675fc03e1
Eugene Groshev <eugene.groshev@gmail.com>
2014-02-17 18:58:35 +0100

    Added literal and arithmetic expressions to evalExp

--- Lab2/Interpreter.hs
+++ Lab2/Interpreter.hs
@@ -10,11 +10,12 @@ import ParCPP
 import PrintCPP
 import ErrM
 
-data Value = VInt Integer | VDouble Double | VUndef
+--data Value = VInt Integer | VDouble Double | VUndef
 
 instance Show Value where
     show (VInt i)    = show i
     show (VDouble d) = show d
+--    show (VBool b)   = show b
     show VUndef      = "undefined"
 
 interpret :: Program -> IO ()
@@ -36,18 +37,53 @@ execStm env s =
       SPrint e        -> do print (evalExp env e)
                             return env
 
-evalExp :: Env -> Exp -> Value
-evalExp env e = 
-    case e of
-      EVar x         -> lookupVar env x
-      EInt i         -> VInt i
-      EDouble d      -> VDouble d
-      EAdd e1 e2     -> let v1 = evalExp env e1
-                            v2 = evalExp env e2
-                         in case (v1,v2) of
-                              (VInt i1, VInt i2)       -> VInt (i1+i2)
-                              (VDouble d1, VDouble d2) -> VDouble (d1+d2)
+evalExp :: Env -> Exp -> IO (Value, Env)
+--evalExp env EId x   = lookupVar env x
 
+
+
+-- literal values
+evalExp env ETrue     = return (VInt 1, env)
+evalExp env EFalse    = return (VInt 0, env)
+evalExp env EInt i    = return (VInt i, env)
+evalExp env EDouble d = return (VDouble d, env)
+-- variables
+-- -- Err "uninitialized variable x"
+-- unary operations w/side effects
+evalExp env EPIncr e     = case (evalExp env e) of
+                            (VInt v) -> VInt (i1+1)
+evalExp env EPDecr e     =
+evalExp env EIncr e     =
+evalExp env EDecr e     =
+  
+-- binary arithmetic operations
+evalExp env EPlus  e1 e2 = let v1 = evalExp env e1
+                               v2 = evalExp env e2
+                           in case (v1,v2) of
+                             (VInt i1, VInt i2)       -> return (VInt (i1+i2), env)
+                             (VDouble d1, VDouble d2) -> return (VDouble (d1+d2), env)
+evalExp env EMinus e1 e2 = let v1 = evalExp env e1
+                               v2 = evalExp env e2
+                           in case (v1,v2) of
+                             (VInt i1, VInt i2)       -> return (VInt (i1-i2), env)
+                             (VDouble d1, VDouble d2) -> return (VDouble (d1-d2), env)
+evalExp env ETimes e1 e2 = let v1 = evalExp env e1
+                               v2 = evalExp env e2
+                           in case (v1,v2) of
+                             (VInt i1, VInt i2)       -> return (VInt (i1*i2), env)
+                             (VDouble d1, VDouble d2) -> return (VDouble (d1*d2), env)
+                             -- add catch-all
+evalExp env EDiv   e1 e2 = let v1 = evalExp env e1
+                               v2 = evalExp env e2
+                           in case (v1,v2) of
+                             (VInt i1, VInt i2)       -> return (VDouble (i1/i2), env)
+                             (VDouble d1, VDouble d2) -> return (VDouble (d1/d2), env)
+                             -- add catch-all
+-- comparison operators
+evalExp env ELt   e1 e2     =                              
+evalExp env EGt   e1 e2     =
+evalExp env ELtEq e1 e2     =
+  
 type Env = [[(Ident, Value)]]
 
 emptyEnv :: Env
