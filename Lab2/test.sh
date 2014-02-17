9cc1d0e388de137dd2abaf5d854d2802e6201249
Eugene Groshev <eugene.groshev@gmail.com>
2014-02-17 17:11:17 +0100

    Replaced old makefile, updated script, cleaned trash

--- Lab2/test.sh
+++ Lab2/test.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-TESTS='testsuite/good/core001.cc'
 GOODS='testsuite/good'
 BADS='testsuite/bad'
 
-# 2, 3, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19, 20,
-# 102, 
-bnfc --make CPP.cf
+#bnfc --make CPP.cf
 
 make all
 
-ghc --make -o lab2 lab2.hs
+#ghc --make -o lab2 lab2.hs
 
 echo 'Type checking good tests...'
-for FILE in `find $GOODS | grep -v -e '.cc.output' -e '.cc.input'`; do
+for FILE in `find $GOODS -name '*.cc'`; do
     echo `basename $FILE && ./lab2 $FILE`
     
 done
 
 echo 'Type checking bad tests...'
-for FILE in `find $BADS | grep -v -e '.cc.output' -e '.cc.input'`; do
+for FILE in `find $BADS -name '*.cc'`; do
     echo `basename $FILE && ./lab2 $FILE`
     
 done
