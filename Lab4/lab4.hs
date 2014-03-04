b6f66a2ff13cbe8550e73204669e9e1f37f96a39
trollol <emilbzm@gmail.com>
2014-03-04 16:22:13 +0100

    forgot to add lab4.hs

---
+++ Lab4/lab4.hs
@@ -0,0 +1,35 @@
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+
+import AbsFP
+import LexFP
+import ParFP
+import ErrM
+import PrintFP
+--import TypeChecker
+--import Interpreter
+
+-- driver
+
+check :: String -> IO () 
+check s = case pProgram (myLexer s) of
+            Bad err  -> do putStrLn "SYNTAX ERROR"
+                           putStrLn err
+                           exitFailure 
+                           
+            Ok  tree -> do 
+                        putStrLn ( show tree)
+                        putStrLn "Parse OK"
+                        --case typecheck tree of
+                        --  Bad err -> do putStrLn "TYPE ERROR"
+                        --                putStrLn err
+                        --                exitFailure 
+                        --  Ok _ -> interpret tree
+
+main :: IO ()
+main = do args <- getArgs
+          case args of
+            [file] -> readFile file >>= check
+            _      -> do putStrLn "Usage: lab4 <SourceFile>"
+                         exitFailure
+
