220192ab0c0df0563bfe71778aee1535030f2155
Eugene Groshev <eugene.groshev@gmail.com>
2014-03-06 15:38:14 +0100

    Interpreter skeleton

--- Lab4/IntFP.hs
+++ Lab4/IntFP.hs
@@ -3,11 +3,41 @@ module Interpreter where
 -- We need to use separate environments phi and gamma,
 -- or use closures (one env. is enough in this case)
 
--- Closure cand be specified in the grammar
+-- Closure can be specified in the grammar
 
 import AbsFP
 import PrintFP
 
-data Value = VInt Int | VCl -- or just closures
+data Funs = 
+data Vars = Map Ident Value
+
+data Value = VInt Int
+           | VClosure Exp Env  -- or just closures
 
 
+-- lookup
+   -- overshadowing
+                        
+-- update
+
+
+-- eval
+   -- 1. build function table
+                        
+   -- 2. evaluate main
+eval exp env =
+  case exp of
+    -- integer literals
+    EInt i -> VInt i -- optional empty env.
+    -- variables
+    EVar
+    
+    EAdd
+    ESub
+    ELt
+    EIf
+    ELmb
+    EApp
+    -- call-by-name
+    -- call-by-value
+    
