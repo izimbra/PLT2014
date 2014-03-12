0ce3bf5b324e31832cf21bf4375436d2a934bb51
Eugene Groshev <eugene.groshev@gmail.com>
2014-03-12 12:53:50 +0100

    Added AbsFP.hs, removed BNFC call from default make, values = expressions

--- Lab4/Interpreter.hs
+++ Lab4/Interpreter.hs
@@ -6,90 +6,95 @@ module Interpreter where
 -- Closure can be specified in the grammar
 
 import Prelude hiding (lookup)
+import Foreign.Marshal.Utils (fromBool)
 import qualified Data.Map as M
 
 import AbsFP
 import PrintFP
 
-type Name = String
-
--- | Function symbol table
-type Funs = M.Map Name Exp
-
--- | Local variable storage
-type Vars = M.Map Name Value
 
 
-data Value = VInt Integer
-           | VClosure Exp Vars  -- or just closures
+-- data Value = VInt Integer
+---            | VClosure Exp Vars  -- or just closures
 
 
 interpret :: Program -> IO ()
-interpret (Prog defs) = let funs = funTable defs                            
-                            vars = M.empty
-                            main = lookup "main" (funs,vars)
+interpret (Prog defs) = let funs          = funTable defs                            
+                            vars          = M.empty
+                            (ECls main _) = lookup "main" (funs,vars)
+                            result        = eval main (funs,vars)
                         in do putStrLn ""
                               putStrLn $ show funs  
                               putStrLn ""
-                              case main of
-                                VClosure exp vars' -> let v = eval exp (funs,vars')
-                                                    in  do case v of
-                                                             EInt i -> putStrLn $ show i
-                                                             _      -> error "Bad result"
+--                              case main of
+--                                VClosure exp vars' -> let v = eval exp (funs,vars')
+                              case result of
+                                EInt i -> putStrLn $ show i
+                                _      -> error $ "Bad result: " ++ show result
                                                            
-                                _                  -> error "Bad main function"
+--                                _                  -> error "Bad main function"
 
 lookup :: Name -> (Funs,Vars) -> Exp
 lookup id (funs,vars) =
   case M.lookup id vars of
-    Just v  -> v
+    Just e  -> e
     Nothing -> case M.lookup id funs of
-                 Just exp -> VClosure exp vars
-                 Nothing  -> error "Lookup failed"
+                 Just e  -> ECls e vars
+                 Nothing -> error $ "Lookup failed: " ++ id ++ " not found"
             
    -- overshadowing: function < variable < inner variable
    -- error: not found                              
                         
 -- update :: Env -> Ident -> Value
+update :: Vars -> Name -> Exp -> Vars
+update env id v = M.insert id v env
+-- M.union v1 v2 - v1 has priority
 
-
-  
-   -- 2. evaluate `main`
-
+evalOperands :: Exp -> Exp -> (Funs,Vars) -> (Integer,Integer)
+evalOperands e1 e2 (funs,vars) =
+   let (EInt i1) = eval e1 (funs, vars)
+       (EInt i2) = eval e2 (funs, vars)
+   in  (i1,i2)
+                  
 -- | Evaluate an expression
 eval :: Exp -> (Funs,Vars) -> Exp
 eval exp (funs,vars) =
   case exp of
     -- integer literals
     EInt i -> exp -- base case -- optional empty env.
-    EAdd e1 e2 -> let (EInt v1) = eval e1 (funs, vars)
-                      (EInt v2) = eval e2 (funs, vars)
-                  in  (EInt (v1+v2))
-    EApp e1 e2 -> let f = eval e1
-                      a = eval e2
+    -- binary operations
+    EAdd e1 e2 -> let (i1,i2) = evalOperands e1 e2 (funs, vars)
+                  in  EInt (i1+i2)
+    ESub e1 e2 -> let (i1,i2) = evalOperands e1 e2 (funs, vars)
+                  in  EInt (i1-i2)
+    ELt  e1 e2 -> let (i1,i2) = evalOperands e1 e2 (funs, vars)
+                  in  EInt $ fromBool (i1<i2)
+    
+    EApp e1 e2 -> let f = eval e1 (funs, vars) -- ECls
+                      a = eval e2 (funs, vars) -- EInt 
                   in  case f of
                         -- match on closure or ident
 
-                        ECls Exp env -> ECls Exp update env
-                        _            -> error "Bad app"
+                        ECls (EAbs (Ident id) e) env -> ECls e (update env id a) --e update env
+                        _            -> error $ "Bad app: " ++ show f
 
     -- variables
-                            
-    EId name -> let exp = lookup name funs
-                in  ECls exp M.empty
-                    -- construct empty env
+    EId (Ident id) -> lookup id (funs,vars) -- EInt or ECls
+                                    -- construct empty env
     
-    -- EAdd
-    -- ESub
-    -- ELt
+
+
+
     -- EIf
-    -- EApp
+
     -- call-by-name
     -- call-by-value
 
-    ECls exp env -> case exp of
-                      EAbs -> undefined -- update env, shrink lambda
-                      _    -> eval exp
+    -- ECls exp env -> case exp of
+    --                   EAbs -> undefined -- update env, shrink lambda
+    --                   _    -> eval exp
+
+
 
 -- | Constructs function symbol table             
 funTable :: [Def] -> Funs
