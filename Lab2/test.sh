499dfc691a4e8d110f7db6ad9b2dd7a3f0240256
Eugene Groshev <eugene.groshev@gmail.com>
2014-03-14 18:48:13 +0100

    Lab3: Removed calls to `lab2` from `test.sh`

--- Lab2/test.sh
+++ Lab2/test.sh
@@ -5,20 +5,18 @@ BADS='testsuite/bad'
 
 #bnfc --make CPP.cf
 
-#make all
+make comp
 
 #ghc --make -o lab2 lab2.hs
 
 #echo 'Type checking good tests...'
-#for FILE in `find $GOODS -name '*.cc'`; do
-#    echo `basename $FILE && ./lab2 $FILE`
-#    echo `basename $FILE && ./lab3 $FILE`
-    
-#done
+for FILE in `find $GOODS -name '*.cc'`; do
+    echo `basename $FILE && ./lab3 $FILE`
+   
+done
 
 echo 'Type checking bad tests...'
 for FILE in `find $BADS -name '*.cc'`; do
-    echo `basename $FILE && ./lab2 $FILE`
     echo `basename $FILE && ./lab3 $FILE`
     
 done
