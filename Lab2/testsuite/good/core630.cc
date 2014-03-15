e8adacece2af7d5c45be988a569f62be59204b93
Emil <emilbzm@gmail.com>
2014-03-15 16:51:49 +0100

    added testcases 630 , 631 for boolean EOr. both pass

---
+++ Lab2/testsuite/good/core630.cc
@@ -0,0 +1,22 @@
+int main() {
+bool apa = true || false;
+//bool bepa = false;
+
+
+//if (apa) { printInt(1);} else {printInt (0);}
+//if (apa  || apa)  {printInt (1);} else {printInt(0);}
+//if (bepa || apa)  {printInt (1);} else {printInt(0);}
+//if (apa  || bepa) {printInt (1);} else {printInt(0);}
+//if (bepa || bepa) {printInt (1);} else {printInt(0);}
+
+//if ( (bepa || bepa) || apa) {printInt (1);} else { printInt (0);}
+
+
+
+
+
+
+  return 0 ;
+
+}
+
