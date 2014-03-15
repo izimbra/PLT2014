df19fa7fa531c5117467c87b17a1bfa9901d8d92
Eugene Groshev <eugene.groshev@gmail.com>
2014-03-15 22:51:48 +0100

    Lab3: removed syntax tree printouts

--- Lab2/lab3.hs
+++ Lab2/lab3.hs
@@ -11,19 +11,6 @@ import ErrM
 import TypeCheckerA
 import Compiler
 
--- driver
-
--- check :: String -> IO () 
--- check s = case pProgram (myLexer s) of
---             Bad err  -> do putStrLn "SYNTAX ERROR"
---                            putStrLn err
---                            exitFailure 
---             Ok  tree -> case typecheck tree of
---                           Bad err -> do putStrLn "TYPE ERROR"
---                                         putStrLn err
---                                         exitFailure 
---                           --Ok _ -> putStrLn "Ok" --
---                           Ok _ -> compile s tree -- for testing w/o interpreter
 
 comp :: String -> String -> IO ()
 comp file s = case pProgram (myLexer s) of
@@ -31,19 +18,13 @@ comp file s = case pProgram (myLexer s) of
                              putStrLn err
                              exitFailure
               Ok  tree -> do
-                putStrLn (printTree tree)
                 case typecheck tree of
                             Bad err  -> do putStrLn "TYPE ERROR"
                                            putStrLn err
                                            exitFailure 
                             Ok tree' -> let className = dropExtension.takeFileName $ file
                                             newFile   = (dropExtension file) ++ ".j"
-                                           -- use tree' for annotated type checker
                                         in  do putStrLn "Type checking done"
---                                               putStrLn $ show tree
-                                               putStrLn $ "\n\n---\n\n"
-                                               putStrLn $ show tree'
-                                               putStrLn $ "\nEND OF TREE\n"
                                                writeFile newFile $ compile className tree'
                                                putStrLn $ "Generated file " ++ newFile
 
@@ -51,22 +32,10 @@ main :: IO ()
 main = do args <- getArgs
           case args of
             [file] -> do readFile file >>= comp file
-                         -- source   <- readFile file
---                         let fileName = dropExtension.takeFileName $ file 
-                         -- comp fileName source
+
                          system ("java -jar jasmin.jar " ++ (dropExtension file) ++ ".j")
                          return ()
             _      -> do putStrLn "Usage: lab3 <SourceFile>"
                          exitFailure
 
--- main :: IO ()
--- main = do args <- getArgs
---           case args of
---             [file] -> do
---                                                    s <- readFile file
---                                                                   comp "Foo" s               --- name Foo instead of (takeWhile (/='.') file)
---                system "java -jar jasmin.jar Foo.j"
---                                    return ()
---                                                _      -> do putStrLn "Usage: lab3 <SourceFile>"
---                          exitFailure
 
